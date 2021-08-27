feaa6162872d07d07a72d12eeb5df560539938e5
MarianaGiraldo <mgiraldo594@misena.edu.co>
2021-08-27 11:58:38 -0500

    Migration create table Productos

---
+++ database/migrations/2021_08_27_163450_create_table__productos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProductos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->set('tipo', ['Aseo', 'Alimentos frescos', 'Alimentos congelados', 'Bebidas', 'Empacados', 'Basicos del hogar', 'Cuidado Personal'] );
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table__productos');
+    }
+}
