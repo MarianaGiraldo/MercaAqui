1a19193ff56c4d481904e70b9d0c555c8c60dbd9
MarianaGiraldo <mgiraldo594@misena.edu.co>
2021-09-03 13:54:37 -0500

    Relación tablas Ventas Productos

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Venta;
 
 class Producto extends Model
 {
     use HasFactory;
+    public function Venta(){
+        return $this->belongsToMany(Venta::class);
+    }
 }
