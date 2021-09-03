ccb7fe4f8efde30834e19caa6a362680b7f1b861
Tenoga <castroherreradavid@gmail.com>
2021-09-03 11:39:44 -0500

    views

--- resources/views/usuarios/show.blade.php
+++ resources/views/usuarios/show.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.app')
 @section('content')
-
+<div class="row">
+        <div class="col">
+            <h1>Información del usuario {{$user->id}} </h1>
+        </div>
+    <a href="/usuarios" class="waves-effect waves-light btn">Regresar</a>
+</div>        
+    
 @endsection
