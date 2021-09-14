d69048976169646605ce0b276d0017e1997053d2
MarianaGiraldo <mgiraldo594@misena.edu.co>
2021-09-14 09:22:07 -0500

    Show products style

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -1,29 +1,36 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="col s12">
-        <h2 class="header center-align"> {{$producto->nombre}} </h2>
-        <div class="row card w-50 mx-auto">
-            <div class="card-image col s6">
-                <img src="/imagenes/productos/{{$producto->imagen}}" width="90" height="150">
+<div class="parallax-container mt-0">
+    <div class="parallax-index">
+        <br>
+        <br>
+        <br>
+        <div class="card-panel orange lighten-3 w-50 m-auto p-1 rounded h-50">
+            <div class="col s12">
+                <h2 class="header center-align"> {{$producto->nombre}} </h2>
+                <div class="row card w-100 mx-auto">
+                    <div class="card-image col s4">
+                        <img src="/imagenes/productos/{{$producto->imagen}}">
+                    </div>
+                    <div class="card-stacked">
+                        <div class="card-content">
+                            <p>Nombre: {{$producto->nombre}} </p>
+                            <p>Tipo: {{$producto->tipo}} </p>
+                            <p>Precio: {{$producto->precio}} </p>
+                            <p>Tipo: {{$producto->tipo}} </p>
+                            <p>Cantidad Disponible: {{$producto->cantidad_disponible}} </p>
+                        </div>
+                        <div class="card-action">
+                            <a href="/productos/{{$producto->id}}/edit" class="btn blue lighten-3">Editar</a>
+                            <a href="/productos/{{$producto->id}}/drop" class="btn amber lighten-3">Eliminar</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="card-stacked">
-                <div class="card-content">
-                    <p>Nombre: {{$producto->nombre}} </p>
-                    <p>Tipo: {{$producto->tipo}} </p>
-                    <p>Precio: {{$producto->precio}} </p>
-                    <p>Tipo: {{$producto->tipo}} </p>
-                    <p>Cantidad Disponible: {{$producto->cantidad_disponible}} </p>
-                </div>
-                <div class="card-action">
-                    <a href="/productos/{{$producto->id}}/edit">Editar</a>
-                    <a href="/productos/{{$producto->id}}/drop">Eliminar</a>
-                </div>
+            <div class="col-md-6 offset-md-5">
+                <a href="/productos" class="waves-effect waves-light light-blue lighten-2 btn">{{ __('Regresar') }}</a>
             </div>
         </div>
-    </div>
-    <div class="col-md-6 offset-md-5">
-        <a href="/productos" class="waves-effect waves-light light-blue lighten-2 btn">{{ __('Regresar') }}</a>
     </div>
 </div>
 @endsection
