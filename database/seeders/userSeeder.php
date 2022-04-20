c2083f754dec7a37f265c5f6512bdef9269d860c
unknown <SENA@MOSAPRCBAFSD076.FormacionSena.RED>
2022-04-20 09:33:59 -0500

    Users

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Str;
+
 
 
 class userSeeder extends Seeder
@@ -24,6 +26,28 @@ class userSeeder extends Seeder
             };
         };
 
+        //-------------------Usuario felipe--------------//
+        User::Create([
+            'nombre' => 'David Felipe' ,
+            'apellido' => 'Castro Herrera',
+            'celular' => '3192097403',
+            'fecha_nacimiento' => random_int(10, 120),
+            'email' => 'castroherreradavid@gmail.com',
+            'password' => bcrypt('changeme'),
+            'is_admin' => '1',
+            'remember_token' => Str::random(10), 
+        ])->assignRole('Admin');
 
+        //-------------------Usuario Mariana--------------//
+        User::Create([
+            'nombre' => 'Mariana' ,
+            'apellido' => 'Giraldo Luna',
+            'celular' => '3192097403',
+            'fecha_nacimiento' => random_int(10, 120),
+            'email' => 'mariana@gmail.com',
+            'password' => bcrypt('changeme'),
+            'is_admin' => '1',
+            'remember_token' => Str::random(10), 
+        ])->assignRole('Admin');
     }
 }
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -65,11 +65,16 @@ class ComposerAutoloaderInitf057eee4f86840e212aee0a8df7d624a
     }
 }
 
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
 function composerRequiref057eee4f86840e212aee0a8df7d624a($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
         $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
