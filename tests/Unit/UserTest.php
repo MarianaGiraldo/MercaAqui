1925e1ae19c18d7372c8eb1c4f65a008a5b2cf37
Tenoga <castroherreradavid@gmail.com>
2022-06-07 10:33:42 -0500

    .

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -41,13 +41,7 @@ class UserTest extends TestCase
     {
         $user = new User();
         $user->id = 1;
-        $user->nombre = 'David Felipe ';
-        $user->apellido = 'Castro Herrera';
-        $user->celular = '3192154875';
-        $user->fecha_nacimiento = '1997-12-11';
-        $user->correo = 'castroherreradavid@gmail.com';
-        $user->password = 'changeme';
-        $lista_users[] = $user;
+        $user->nombre = 'David Felipe ';     
 
         $data = (new UserController)->createNewUser(null, true);
         $this->assertSame($user->nombre,  $data->nombre);
