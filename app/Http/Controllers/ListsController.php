d0b7eec4a384394ff751162bc79d962f391230a1
Mariana Giraldo <mgiraldo594@misena.edu.co>
2022-06-17 08:23:13 -0500

    toJson

--- app/Http/Controllers/ListsController.php
+++ app/Http/Controllers/ListsController.php
@@ -13,22 +13,16 @@ class ListsController extends Controller
 
     public function getVentasList (Request $request)
     {
-        $ventas = Venta::all()->toArray();
-
-        return json_encode($ventas);
+        return Venta::all()->toJson();
     }
 
     public function getProductsList (Request $request)
     {
-        $products = Producto::all()->toArray();
-
-        return json_encode($products);
+        return Producto::all()->toJson();
     }
 
     public function getUsersList (Request $request)
     {
-        $users = User::all()->toArray();
-
-        return json_encode($users);
+        return User::all()->toJson();
     }
 }
