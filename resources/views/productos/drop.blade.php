82945f5284fecb52692f10800b96fa432b1bf78d
MarianaGiraldo <mgiraldo594@misena.edu.co>
2022-06-23 22:32:29 -0500

    Drop views

--- resources/views/productos/drop.blade.php
+++ resources/views/productos/drop.blade.php
@@ -2,10 +2,10 @@
 @section('content')
     @role('Admin')
         <div>
-            <div class="parallax-index">
+            <div class="parallax-index" style="height: 100vh;">
                 <br><br><br>
                 <div class="card-panel w-50 m-auto  rounded h-auto py-4" style="background-color: #71A9F7">
-                    <h1 class="header center-align white-text display-4">Borrar producto {{ $dropProduct->nombre }} </h1>
+                    <h1 class="header center-align white-text display-6" style="font-size:2.4rem">Borrar producto {{ $dropProduct->nombre }} </h1>
                     <div class="row container m-auto w-100">
                         <form action="{{ route('productos.destroy', $dropProduct->id) }}" method="POST"
                             class="w-100 text-center">
@@ -17,9 +17,9 @@
                             </div>
                             <div class="card-action">
                                 <div class="form-group row mx-3 my-0 p-2">
-                                    <div class="col-sm-12">
+                                    <div class="col-sm-12 align-content-around">
                                         <a href="/productos/{{ $dropProduct->id }}"
-                                            class="waves-effect waves-light btn text-white mx-2"
+                                            class="waves-effect waves-light btn text-white mr-2 my-2"
                                             style="background-color: #FF9B42">Regresar</a>
                                         <button type="submit" class="waves-effect waves-light btn btn-danger text-white mx-2">
                                             {{ __('Eliminar') }}
--- resources/views/usuarios/drop.blade.php
+++ resources/views/usuarios/drop.blade.php
@@ -2,10 +2,10 @@
 @section('content')
     @role('Admin')
         <div>
-            <div class="parallax-index">
+            <div class="parallax-index" style="height: 100vh;">
                 <br><br><br>
                 <div class="card-panel w-50 m-auto  rounded h-auto py-4" style="background-color: #71A9F7">
-                    <h1 class="header center-align white-text display-4">Borrar vendedor {{ $dropUser->nombre }} </h1>
+                    <h1 class="header center-align white-text display-6" style="font-size:2.4rem">Borrar vendedor {{ $dropUser->nombre }} </h1>
                     <div class="row container m-auto w-100">
                         <form action="{{ route('usuarios.destroy', $dropUser->id) }}" method="POST"
                             class="w-100 text-center">
@@ -19,7 +19,7 @@
                                 <div class="form-group row mx-3 my-0 p-2">
                                     <div class="col-sm-12">
                                         <a href="/usuarios/{{ $dropUser->id }}"
-                                            class="waves-effect waves-light btn text-white mx-2"
+                                            class="waves-effect waves-light btn text-white mr-2 my-2"
                                             style="background-color: #FF9B42">Regresar</a>
                                         <button type="submit" class="waves-effect waves-light btn btn-danger text-white mx-2">
                                             {{ __('Eliminar') }}
